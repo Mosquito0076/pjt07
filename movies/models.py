0533a4b7a40335982a829e92360d3a684bfc5ddf
mosquito0076 <hsmk0076@naver.com>
2022-04-15 13:49:51 +0900

    0415

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Comment(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return self.content
