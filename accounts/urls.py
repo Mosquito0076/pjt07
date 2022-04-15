f75a3c65b6e8000dd514aca8bf65d32bcc428e80
jongil512 <jongil519@gmail.com>
2022-04-15 16:34:48 +0900

    accounts update

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('update/', views.update, name='update'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
